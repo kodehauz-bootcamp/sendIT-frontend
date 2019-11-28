1d74128df2159aab14f02283e828f7febd623700
odomfavour <ogochukwuodom@gmail.com>
2019-11-28 17:28:36 +0100

    F/E dashboard design for admin implemented

--- js/script.js
+++ js/script.js
@@ -221,3 +221,25 @@ window.onload = function () {
   chart.render();
   
   }
+
+
+
+  // add user
+
+  function productsAdd() {
+    $("#productTable tbody").append(
+        "<tr>" +
+          "<td>My First Video</td>" +
+          "<td>6/11/2015</td>" +
+          "<td>www.pluralsight.com</td>" +
+        "</tr>"
+    );
+  }
+
+  if ($("#productTable tbody").length == 0) {
+    $("#productTable").append("<tbody></tbody>");
+  }
+
+  $(document).ready(function () {
+    productsAdd();
+  });
