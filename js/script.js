9f61be38f0ecc96cd878225d95922f9e052dc1e6
Orung <magnificentorung@gmail.com>
2019-11-29 12:42:36 +0100

    FT/Sendit User Dashboard Implemented (#19)
    * ft/Updated Sendit landing page and about us page
    
    * ft/Sendit landing and about page updated with a template
    
    * ft/Sendit landing page and about page updated with reuseable template
    
    * ft/page active link implemented
    
    * ft/sendit tracking page implemented
    
    * ft/Sendit Dashboard implemented
    
    * ft/Update made on the dashboard
    
    * ft/Sendit dashboard page update
    
    * ft/tracking page check
    
    * ft/Sendit User Dashboard Implemented
    
    * ft/Sendit User Dashboard Immplemented
    
    * ft/Sendit User Dashboard Implemented

--- js/script.js
+++ js/script.js
@@ -221,3 +221,23 @@ window.onload = function () {
   chart.render();
   
   }
+
+// file upload in edit profile picture
+  // $(document).ready(function() {    
+  //   var readURL = function(input) {
+  //       if (input.files && input.files[0]) {
+  //           var reader = new FileReader();
+
+  //           reader.onload = function (e) {
+  //               $('.avatar').attr('src', e.target.result);
+  //           }
+
+  //                 reader.readAsDataURL(input.files[0]);
+  //             }
+  //         }
+
+
+  //         $(".file-upload").on('change', function(){
+  //             readURL(this);
+  //         });
+  //         });
